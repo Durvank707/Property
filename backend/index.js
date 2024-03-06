7398c31260ba75b9dcbd4b57f2407d538c3203b5
Durvank <127382184+Durvank707@users.noreply.github.com>
2024-03-07 02:07:04 +0530

    navbar completed and backend model,routes, initial things are setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+require('dotenv').config();
 
 const app = express();
+
+const dbConnect = require('./config/database');
+dbConnect()
+
+const Routes = require('./routes/user.route.js')
+app.use("/api/v1", Routes)
+
 
 app.listen(3000, ()=>{
     console.log("Listening to 3000!!")
