28e41b5f4c5fa3359ae9d1cd4bc629366fc5afaa
Durvank <127382184+Durvank707@users.noreply.github.com>
2024-03-10 02:50:04 +0530

    signin routes created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model.js");
 const bcryptjs = require('bcryptjs');
+const { errorHandler } = require("../utils/error.js");
+const jwt = require('jsonwebtoken')
 
 exports.signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -10,15 +12,45 @@ exports.signup = async (req, res, next) => {
         res.status(201).json('User created successfully!');
     } catch (error) {
         if (error.code === 11000 && error.keyPattern && error.keyPattern.username) {
-            
+
             //In MongoDB, error code 11000 corresponds to a duplicate key error.
             //The keyPattern is MongoDB's way of indicating which index or constraint caused the duplicate key error.
-            
+
             return res.status(400).json({
                 success: false,
                 message: "Username is already taken",
             });
         }
+        next(error);
+    }
+};
+exports.signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+
+        if (!validUser) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found!',
+            });
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if (!validPassword) {
+            return res.status(401).json({
+                success: false,
+                message: 'Incorrect password!',
+            });
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+
+    } catch (error) {
         next(error);
     }
 };
