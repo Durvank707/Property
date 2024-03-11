62660c0c368ec67e084f6b25e36d822eb846ff0f
Durvank <127382184+Durvank707@users.noreply.github.com>
2024-03-12 01:41:15 +0530

    minor changes in redux store, persistStore added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom"
-import {store} from "./redux/store.js"
-import {Provider} from "react-redux"
+import { BrowserRouter } from "react-router-dom"
+import { store, persistor } from "./redux/store.js"
+import { Provider } from "react-redux"
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
